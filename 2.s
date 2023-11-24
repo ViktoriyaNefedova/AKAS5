a80513b37ac130f076ec9f4ea05ac02384e2899a
ViktoriyaNefedova <113330548+ViktoriyaNefedova@users.noreply.github.com>
2023-11-24 20:08:43 +0300

    Update 2.s

--- 2.s
+++ 2.s
@@ -1,40 +1,28 @@
 section .data
-	input_msg db 'Entering a line: '
-	output_msg db 'Line output: '
-	buffer db 64
+    buffer equ 256 ; Размер буфера для строки       
+
+section .bss
+    input resb buffer ; Выделение памяти под входную строку   
 
 section .text
-	global _start
+    global _start
 
 _start:
-	; выводим сообщение для ввода строки
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, input_msg
-	mov edx, 17
-	int 0x80
+    ; Чтение строки из stdin
+    mov eax, 3 ; Системный вызов для чтения          
+    mov ebx, 0 ; Стандартный ввод         
+    mov ecx, input ; Буфер для чтения      
+    mov edx, buffer  ; Размер буфера     
+    int 0x80             
 
-	; считываем строку из stdin
-	mov eax, 3
-	mov ebx, 0
-	mov ecx, buffer
-	mov edx, 64
-	int 0x80
+    ; Вывод строки в stdout
+    mov eax, 4  ; Системный вызов для записи         
+    mov ebx, 1  ; Стандартный вывод         
+    mov ecx, input ; Адрес буфера для записи   
+    mov edx, buffer  ; Размер буфера    
+    int 0x80             
 
-	; выводим сообщение с введенной строкой
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, output_msg
-	mov edx, 13
-	int 0x80
-
-	; выводим введенную строку
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, buffer
-	int 0x80
-
-	; завершаем программу
-	mov eax, 1
-	xor ebx, ebx
-	int 0x80
+    ; Выход из программы
+    mov eax, 1  ; Системный вызов для выхода         
+    xor ebx, ebx    ; Возврат кода завершения 0     
+    int 0x80             
